0772cca59d6998247891907be9255a3bec1b0d26
Tyuiu.DevjatkovaAA@outlook.com <annastaia1414@gmail.com>
2023-11-17 12:02:48 +0500

    Task2

---
+++ Tyuiu.DevjatkovaAA.Sprint5.Task2.V26.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.DevjatkovaAA.Sprint5.Task2.V26.Lib;
+using System.IO;
+
+namespace Tyuiu.DevjatkovaAA.Sprint5.Task2.V26.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = @"C:\Users\ДНС\source\repos\Tyuiu.DevjatkovaAA.Sprint5\Tyuiu.DevjatkovaAA.Sprint5.Task2.V26\bin\Debug\OutPutFileTask2.csv";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+
+            bool wait = true;
+
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
