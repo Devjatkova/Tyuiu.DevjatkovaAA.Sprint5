8649a7a09dc608ed6107603e1e8c9cb7cb426ed3
Tyuiu.DevjatkovaAA@outlook.com <annastaia1414@gmail.com>
2023-11-26 15:39:06 +0500

    Task6

---
+++ Tyuiu.DevjatkovaAA.Sprint5.Task6.V18.Lib/DataService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+
+namespace Tyuiu.DevjatkovaAA.Sprint5.Task6.V18.Lib
+{
+    public class DataService : ISprint5Task6V18
+    {
+        public int LoadFromDataFile(string path)
+        {
+            int count = 0;
+            string p = File.ReadAllText(path);
+            string[] line = p.Split();
+            int a;
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (int.TryParse(line[i], out a) && line[i].Length == 1)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Tyuiu.DevjatkovaAA.Sprint5.Task6.V18.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.DevjatkovaAA.Sprint5.Task6.V18.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.DevjatkovaAA.Sprint5.Task6.V18.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("16ba2ae2-6379-48b8-b0db-ddf1c272b830")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
