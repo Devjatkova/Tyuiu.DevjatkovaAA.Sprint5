2ce157f4bcd1b8e9f2dbee9d9b943923e3ab2d54
Tyuiu.DevjatkovaAA@outlook.com <annastaia1414@gmail.com>
2023-11-26 16:58:13 +0500

    Task7

---
+++ Tyuiu.DevjatkovaAA.Sprint5.Task7.V23.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.DevjatkovaAA.Sprint5.Task7.V23.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.DevjatkovaAA.Sprint5.Task7.V23.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("61572d31-4417-4ae0-90ae-68e52aecf673")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.DevjatkovaAA.Sprint5.Task7.V23/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.DevjatkovaAA.Sprint5.Task7.V23.Lib;
+using System.IO;
+
+namespace Tyuiu.DevjatkovaAA.Sprint5.Task7.V23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнила: Девяткова А. А. | АСОиУБ-23-3";
+            //Длина строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #23                                                             *");
+            Console.WriteLine("* Выполнила: Девяткова Анастасия Александровна | АСОиУБ-23-3              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл С:DataSprint5InPutDataFileTask7V23.txt (файл взять из архива   *");
+            Console.WriteLine("* согласно вашему варианту. Создать папку в ручную С:DataSprint5 и        *");
+            Console.WriteLine("* скопировать в неё файл) в котором есть набор символьных данных.Удалить  *");
+            Console.WriteLine("* все русские слова из файла.Полученный результат сохранить               *");
+            Console.WriteLine("* в файл OutPutDataFileTask7V23.txt.                                      *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = $@"C:\DataSprint5\InPutDataFileTask7V23.txt";
+            string pathSaveLine = $@"C:\DataSprint5\OutPutDataFileTask7V23.txt";
+
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Находится в файле: ");
+            pathSaveLine = ds.LoadDataAndSave(path);
+
+            Console.WriteLine(pathSaveLine);
+            Console.ReadKey();
+        }
+    }
+}
